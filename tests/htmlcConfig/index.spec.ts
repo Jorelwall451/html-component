985528a2fa793feb6c2b65f30a5506498eb97d1e
jorelwall154 <jorelwall154@gmail.com>
2024-02-29 17:27:30 -0300

    add basic sample how needs to work

--- tests/htmlcConfig/index.spec.ts
+++ tests/htmlcConfig/index.spec.ts
@@ -1,8 +1,12 @@
 import htmlcConfig from '../../lib/htmlcConfig';
 
 describe('htmlcConfig', () => {
+	beforeAll(() => {
+		process.cwd = () => __dirname;
+	});
+
 	it('should parse HTMLC config file correctly', async () => {
-		const config = await htmlcConfig(__filename);
+		const config = await htmlcConfig();
 		expect(config).toHaveProperty('pagesPath');
 		expect(config).toHaveProperty('componentsPath');
 	});
