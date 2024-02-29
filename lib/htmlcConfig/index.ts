9206cc787d0bd63d570b779c92067210de6e67ff
jorelwall154 <jorelwall154@gmail.com>
2024-02-29 17:27:49 -0300

    update htmlcConfig

--- lib/htmlcConfig/index.ts
+++ lib/htmlcConfig/index.ts
@@ -1,14 +1,17 @@
 import {readFile} from 'fs/promises';
 import path from 'path';
+import {cwd} from 'process';
 import type HtmlcConfig from '../types/HtmlcConfig';
 
-export default async function htmlcConfig(callingFilePath: string): Promise<HtmlcConfig> {
+export default async function htmlcConfig(): Promise<HtmlcConfig> {
 	const filename = 'htmlcConfig.json';
-	const callingDirectory = path.dirname(callingFilePath);
-	const filePath = path.join(callingDirectory, filename);
+	const filePath = path.join(cwd(), filename);
 
 	try {
-		const fileContent = await readFile(filePath, {encoding: 'utf-8'});
+		const fileContent = await readFile(filePath, {
+			encoding: 'utf-8',
+		});
+
 		const fileContentJson = JSON.parse(fileContent) as HtmlcConfig;
 
 		return {
