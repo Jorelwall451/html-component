e6e9cd3767d1514290d60d950ba757d66cb460c6
jorelwall154 <jorelwall154@gmail.com>
2024-02-28 21:01:32 -0300

    update path error

--- lib/htmlcConfig/index.ts
+++ lib/htmlcConfig/index.ts
@@ -1,18 +1,14 @@
 import {readFile} from 'fs/promises';
 import path from 'path';
-import {cwd} from 'process';
 import type HtmlcConfig from '../types/HtmlcConfig';
 
-export default async function htmlcConfig(): Promise<HtmlcConfig> {
-	const filename = 'htmlcCOnfig.json';
-
-	const filePath = path.join(cwd(), filename);
+export default async function htmlcConfig(callingFilePath: string): Promise<HtmlcConfig> {
+	const filename = 'htmlcConfig.json';
+	const callingDirectory = path.dirname(callingFilePath);
+	const filePath = path.join(callingDirectory, filename);
 
 	try {
-		const fileContent = await readFile(filePath, {
-			encoding: 'utf-8',
-		});
-
+		const fileContent = await readFile(filePath, {encoding: 'utf-8'});
 		const fileContentJson = JSON.parse(fileContent) as HtmlcConfig;
 
 		return {
