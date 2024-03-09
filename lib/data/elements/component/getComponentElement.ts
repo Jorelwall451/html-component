b5610fd95beb403788648a59add185d828632469
jorelwall154 <jorelwall154@gmail.com>
2024-03-09 14:37:22 -0300

    add getNewComponentElement

---
+++ lib/data/elements/component/getComponentElement.ts
@@ -0,0 +1,33 @@
+import type Component from '@lib/types/Component';
+import {readFile} from 'fs/promises';
+
+export default async function getComponentElement(componentPath: string) {
+	const componentFileContent = await readFile(componentPath, {
+		encoding: 'utf-8',
+	});
+
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	const componentDOM = new DOMParser().parseFromString(componentFileContent, 'text/html');
+
+	const componentTag = componentDOM.querySelector('component')!;
+
+	if (!componentTag) {
+		throw new Error(`No have a tagName component in component path ${componentPath}`);
+	}
+
+	const name = componentTag.getAttribute('name');
+	const params = componentTag.attributes;
+	const {childNodes} = componentTag;
+
+	if (!name) {
+		throw new Error(`It's missing the attribute name in component tag at file ${componentPath}`);
+	}
+
+	const result: Component = {
+		name,
+		params,
+		childNodes,
+	};
+
+	return result;
+}
