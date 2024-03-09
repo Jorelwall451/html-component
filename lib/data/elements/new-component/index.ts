706f72312b2241dea8227247cf04a417ae229dcc
jorelwall154 <jorelwall154@gmail.com>
2024-03-09 14:04:06 -0300

    Add getEveryNewComponentElements

---
+++ lib/data/elements/new-component/index.ts
@@ -0,0 +1,17 @@
+import getConfig from '@lib/data/config';
+import type NewComponent from '@lib/types/NewComponent';
+import getNewComponentElement from './getNewComponentElement';
+
+export default async function getEveryNewComponentsElements() {
+	const config = await getConfig();
+
+	const newComponents: Array<Promise<NewComponent>> = [];
+
+	for (const componentPath of config.componentsPath) {
+		const newComponent = getNewComponentElement(componentPath);
+
+		newComponents.push(newComponent);
+	}
+
+	return Promise.all(newComponents);
+}
