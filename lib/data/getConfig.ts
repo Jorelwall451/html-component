d2cb3c2b4892adae53f72c5d78b7eecb9ddbb8f0
jorelwall154 <jorelwall154@gmail.com>
2024-03-11 15:19:06 -0300

    add perfomarce pratices to code

--- lib/data/getConfig.ts
+++ lib/data/getConfig.ts
@@ -1,18 +1,28 @@
 import type Config from '@lib/types/Config';
-import {access} from 'fs/promises';
 import path from 'path';
 import {cwd} from 'process';
 
-export default async function getConfig() {
+let cachedConfig: Config | undefined;
+
+export default async function getConfig(byCache = true) {
+	if (byCache && !cachedConfig) {
+		return;
+	}
+
 	const filename = 'htmlc.config.js';
 	const filepath = path.join(cwd(), filename);
 
 	try {
-		await access(filepath);
-		const fileConfig = await import(filepath) as Config;
+		const config = await import(filepath) as Config;
 
-		return fileConfig;
+		cachedConfig = config;
+
+		return config;
 	} catch (error: any) {
-		throw new Error(`Cannot read the configuration file: ${error}`);
+		if (error.code === 'ENOENT') {
+			throw new Error(`Configuration file not found: ${filepath}`);
+		} else {
+			throw new Error(`Error reading configuration file: ${error.message}`);
+		}
 	}
 }
