84a2b6b27bb13631b771717894d2ebd79a8d22b0
jorelwall154 <jorelwall154@gmail.com>
2024-03-11 15:49:23 -0300

    simplify getConfig

--- lib/data/getConfig.ts
+++ lib/data/getConfig.ts
@@ -2,27 +2,19 @@ import type Config from '@lib/types/Config';
 import path from 'path';
 import {cwd} from 'process';
 
-let cachedConfig: Config | undefined;
-
-export default async function getConfig(byCache = true) {
-	if (byCache && !cachedConfig) {
-		return;
-	}
-
+export default async function getConfig() {
 	const filename = 'htmlc.config.js';
 	const filepath = path.join(cwd(), filename);
 
 	try {
 		const config = await import(filepath) as Config;
 
-		cachedConfig = config;
-
 		return config;
 	} catch (error: any) {
 		if (error.code === 'ENOENT') {
 			throw new Error(`Configuration file not found: ${filepath}`);
-		} else {
-			throw new Error(`Error reading configuration file: ${error.message}`);
 		}
+
+		throw new Error(`Error reading configuration file: ${error.message}`);
 	}
 }
