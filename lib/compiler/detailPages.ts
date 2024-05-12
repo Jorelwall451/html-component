bc703fa768fff3c54b2e2ba067cd987d3d3737d4
jorelwall451 <jorelwall154@gmail.com>
2024-05-12 18:22:55 -0300

    Add detailPages

---
+++ lib/compiler/detailPages.ts
@@ -0,0 +1,33 @@
+import getNewComponentByName from '@lib/data/components/getNewComponentByName';
+import getPages from '@lib/data/pages/getPages';
+
+export default async function detailPages() {
+	const pages = await getPages();
+	const detailedPages: Element[] = [];
+
+	pages.forEach(async page => {
+		const components = page.getElementsByTagName('component');
+
+		for (const componentEl of components) {
+			const componentName = componentEl.getAttribute('name');
+
+			if (!componentName) {
+				console.warn('Don\'t have a name in the component.');
+				componentEl.outerHTML = '';
+				continue;
+			}
+
+			// eslint-disable-next-line no-await-in-loop
+			const newComponent = await getNewComponentByName(componentName);
+
+			if (!newComponent) {
+				console.warn(`Don't have a component name with the name ${componentName}`);
+				continue;
+			}
+
+			componentEl.outerHTML = newComponent.element.innerHTML;
+		}
+	});
+
+	return detailedPages;
+}
