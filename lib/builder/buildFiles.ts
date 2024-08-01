392a16af1bf7c08b3cb4831013f45f1cfb7ee2bc
jorelwall154 <jorelwall154@gmail.com>
2024-08-01 20:33:11 -0300

    add buildFiles - builder

---
+++ lib/builder/buildFiles.ts
@@ -0,0 +1,23 @@
+import {writeFile} from 'fs/promises';
+import detailPages from './detailPages';
+import getConfig from '../data/config';
+import {join} from 'path';
+import {minify} from 'html-minifier';
+
+export default async function buildFiles() {
+	const {outDir} = await getConfig();
+	const detailedPages = await detailPages();
+
+	detailedPages.forEach(async ({filename, content}) => {
+		const filepath = join(outDir, filename);
+
+		const minifiedContent = minify(content, {
+			removeComments: true,
+			collapseWhitespace: true,
+		});
+
+		await writeFile(filepath, minifiedContent, {
+			encoding: 'utf-8',
+		});
+	});
+}
