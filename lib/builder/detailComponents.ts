b29babc1652360f4c9447936df23d0638358ecb9
jorelwall154 <jorelwall154@gmail.com>
2024-08-01 20:32:49 -0300

    add detailComponents - builder

---
+++ lib/builder/detailComponents.ts
@@ -0,0 +1,43 @@
+import getNewComponents from '../data/new-component';
+
+export default async function detailComponents($: cheerio.Root) {
+	const newComponents = await getNewComponents();
+
+	const components = $('component').toArray();
+	const interpolationRegex = /{([^{}]+)}/gm;
+
+	for (const component of components) {
+		const componentName = $(component).attr('name');
+		const newComponent = newComponents.find(newComponent => newComponent.name === componentName);
+
+		if (!newComponent) {
+			throw new Error();
+		}
+
+		let newContent = newComponent.content;
+
+		console.log('-'.repeat(50));
+
+		const matches = newContent.matchAll(interpolationRegex);
+
+		for (const match of matches) {
+			const paramName = match[1];
+
+			if (paramName.startsWith('_')) {
+				continue;
+			}
+
+			// eslint-disable-next-line no-prototype-builtins
+			if (newComponent.params.hasOwnProperty(paramName)) {
+				const paramValue = $(component).attr(`param:${paramName}`);
+				newContent = newContent.replace(match[0], paramValue ?? '');
+			}
+		}
+
+		$(component).replaceWith(newContent);
+	}
+
+	if ($('component').toArray().length !== 0) {
+		await detailComponents($);
+	}
+}
