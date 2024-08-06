e2000a987be5ccfc65ad009acc464daff3ace705
jorelwall154 <jorelwall154@gmail.com>
2024-08-05 21:47:59 -0300

    update getPages

--- lib/data/pages/index.ts
+++ lib/data/pages/index.ts
@@ -1,25 +1,10 @@
-import {readFile} from 'fs/promises';
-import getConfig from '../config';
+import getConfig from '../config/index';
+import processPage from './processPage';
 
 export default async function getPages() {
 	const {pagesPath} = await getConfig();
 
-	const pagesPromises = pagesPath.map(async pagePath => {
-		const filename = pagePath.split('/').pop();
-		const content = await readFile(pagePath, {
-			encoding: 'utf-8',
-		});
-
-		if (!filename) {
-			throw new Error();
-		}
-
-		return {
-			filename,
-			content,
-		};
-	});
-
+	const pagesPromises = pagesPath.map(processPage);
 	const pages = await Promise.all(pagesPromises);
 
 	return pages;
