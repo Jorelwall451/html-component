d8e771fa04f6c5c4fe4ef6300b296ee28ec34c65
jorelwall154 <jorelwall154@gmail.com>
2024-08-08 22:34:24 -0300

    add interpolate params

---
+++ lib/builder/detail-components/interpolateParams.ts
@@ -0,0 +1,21 @@
+import type Component from '../../types/Component';
+
+export default function interpolateParams(newContent: string, componentElement: cheerio.Cheerio, newComponent: Component, $: cheerio.Root) {
+	const interpolationRegex = /{([^{}]+)}/gm;
+	const matches = newContent.matchAll(interpolationRegex);
+
+	for (const match of matches) {
+		const paramName = match[1];
+
+		if (paramName.startsWith('_')) {
+			continue;
+		}
+
+		if (Object.getOwnPropertyDescriptor(newComponent.params, paramName)) {
+			const paramValue = $(componentElement).attr(`param:${paramName}`);
+			newContent = newContent.replace(match[0], paramValue ?? '');
+		}
+	}
+
+	return newContent;
+}
